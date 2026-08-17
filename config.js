77b8b380af5264c29e1ed5a9a8594d71f6c3838e
arbglobalcleansing <arbglobalcleansing@gmail.com>
2026-08-17 16:19:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 // config.js
 const SUPABASE_URL = 'https://xnwttnpfzcjbszwxsgox.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_zEh0y_OAF7nVwLBuTTB74w_gPX0XomI';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
